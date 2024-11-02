a6df815dcae365a7af92942cb9da309a3942f289
Grace Buttler <grace.buttler@gmail.com>
2024-11-02 13:38:00 +0000

    create plan for next steps

--- script.js
+++ script.js
@@ -11,3 +11,11 @@ const alertFunction = () => {
     alert('hello!')
 
 }
+
+//create simple function that alerts when submit is clicked
+//create simple function to display name in demo when submit is clicked
+//add in radio buttons for project type
+//create simple function to display name and project type in demo
+//create sql database that is linked and send data from for to there (as well as displaying most recent data)
+//have a button to call the database data and see all entries, sorted most recent to oldest.
+//add CSS
