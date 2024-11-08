30477b052449b5359b7532baa42fb6ae98fc67e2
Grace Buttler <grace.buttler@gmail.com>
2024-11-08 15:51:56 +0000

    use object to populate html from form

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+let projectObject = {};
+
 //method 1, where you don't have an 'onSubmit' function
 document.getElementById('craftingForm').addEventListener('submit', 
     function(event) {
         event.preventDefault();
-        document.getElementById("projectName").innerHTML = `My project is called ${event.target.name.value}`;
-        document.getElementById("craftType").innerHTML = `I will be ${event.target.medium.value} this project`;
+        projectObject.projectName = event.target.name.value
+        projectObject.projectMedium = event.target.medium.value
+        document.getElementById("projectName").innerHTML = `My project is called ${projectObject.projectName}`;
+        document.getElementById("craftType").innerHTML = `I will be ${projectObject.projectMedium} this project`;
 
 });
 
@@ -13,5 +17,4 @@ document.getElementById('craftingForm').addEventListener('submit',
 //     document.getElementById("projectName").innerHTML = `My name is ${event.target.name.value}`;
 //     console.log(event.target.name.value);
 // }
-
 
